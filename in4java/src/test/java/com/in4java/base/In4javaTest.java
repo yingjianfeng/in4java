2f2d6cf61846267a4a5a43efbb7db43953d2a3d2
应剑锋 <jianfeng.ying@xbongbong.com>
2023-04-23 14:39:55 +0800

    feat:init

---
+++ in4java/src/test/java/com/in4java/base/In4javaTest.java
@@ -0,0 +1,15 @@
+package com.in4java.base;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author: yingjf
+ * @date: 2023/4/23 11:02
+ * @description:
+ */
+public class In4javaTest {
+    @Test
+    void contextLoads() {
+        System.out.println("111");
+    }
+}
