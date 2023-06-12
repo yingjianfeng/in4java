f9f115e835c8249b037b2e5feb3ef91d9a0af858
应剑锋 <jianfeng.ying@xbongbong.com>
2023-06-12 14:26:54 +0800

    feat: 测试

--- in4java/src/test/java/com/in4java/base/In4javaTest.java
+++ in4java/src/test/java/com/in4java/base/In4javaTest.java
@@ -1,21 +1,25 @@
 package com.in4java.base;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Stack;
 
 /**
  * @author: yingjf
  * @date: 2023/4/23 11:02
  * @description:
  */
+@Slf4j
 public class In4javaTest {
 
     @Data
-    class A{
+    class A {
         Integer b;
     }
 
@@ -25,5 +29,40 @@ public class In4javaTest {
         if (object instanceof JSONObject) {
 
         }
+        String x = "xx";
+        char i = 'i';
+        char g = 'g';
+        System.out.println(i);
+        System.out.println(g);
+        System.out.println(i > g);
     }
+
+    public static void main(String[] args) {
+
+        System.out.println(isValid("()"));
+    }
+
+    public static boolean isValid(String s) {
+        Map<String, String> map = new HashMap<>();
+        map.put(")", "(");
+        map.put("}", "{");
+        map.put("]", "[");
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            String temp = String.valueOf(s.charAt(i));
+            if (!map.containsKey(temp)) {
+                stack.push(temp);
+            }
+            if (stack.empty()) {
+                return false;
+            }
+            if (Objects.equals(stack.peek(), map.get(temp))) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+
 }
