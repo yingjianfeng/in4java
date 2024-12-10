32e162885046ba405c3afc925ed0895759a19b11
应剑锋 <jianfeng.ying@xbongbong.com>
2024-12-10 10:58:33 +0800

    fix：4

--- src/test/java/com/in4java/base/MyTest.java
+++ src/test/java/com/in4java/base/MyTest.java
@@ -21,7 +21,7 @@ public class MyTest {
                 .collect(Collectors.toList());
         // 打印转换后的List
         System.out.println(list);
-        // m2
+        // m 2 4
         // h 1 3
     }
 }
