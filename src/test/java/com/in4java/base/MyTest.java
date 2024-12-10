1b1e7fcf7723721598baee8289e784d0362d380f
应剑锋 <jianfeng.ying@xbongbong.com>
2024-12-10 10:57:50 +0800

    fix：

--- src/test/java/com/in4java/base/MyTest.java
+++ src/test/java/com/in4java/base/MyTest.java
@@ -21,7 +21,7 @@ public class MyTest {
                 .collect(Collectors.toList());
         // 打印转换后的List
         System.out.println(list);
-        // m 
+        // m 2
         // h
     }
 }
